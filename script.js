13ca3840a88e2b3dc69862985687ad657b77fb4e
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-07-23 10:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from submitting the default way
+    event.preventDefault();
 
+    // Access the form and its elements
+    var form = document.getElementById("form1");
+    var fname = form.elements["fname"].value;
+    var lname = form.elements["lname"].value;
+
+    // Display an alert with the first name and last name
+    alert(fname + " " + lname);
 }
